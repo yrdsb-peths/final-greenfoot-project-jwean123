eb31dded94a86523a2d73ce0ade6115bf12158cf
jwean123 <150282986+jwean123@users.noreply.github.com>
2024-01-15 22:45:11 -0500

    changed jump force

--- Guy.java
+++ Guy.java
@@ -10,7 +10,7 @@ public class Guy extends Actor
 {
     Actor floor;
     static final int gravity = 2;
-    static final int jumpForce = 22;
+    static final int jumpForce = 25;
     
     int walkSpeed = 5;
     int ySpeed = 0;
