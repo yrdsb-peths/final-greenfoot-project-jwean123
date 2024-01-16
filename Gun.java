a735c885389539ae3ecf5cd5c905e705e9c17eec
jwean123 <150282986+jwean123@users.noreply.github.com>
2024-01-16 12:21:32 -0500

    made good looking gun model with right proportions

---
+++ Gun.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class gun here.
+ * 
+ * @author Juan 
+ * @version January 2024
+ */
+public class Gun extends Actor
+{
+    public Gun()
+    {
+        GreenfootImage gunImage = new GreenfootImage("game rifle.png");
+        gunImage.scale(100, 30);
+        this.setImage(gunImage);
+    }
+    
+    /**
+     * Act - do whatever the gun wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
