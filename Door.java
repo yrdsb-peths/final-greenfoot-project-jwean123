286eeb63a59ae317cc634ad40f1a4ba1266d6632
jwean123 <150282986+jwean123@users.noreply.github.com>
2024-01-17 19:53:38 -0500

    added door sprites

---
+++ Door.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Door here.
+ * 
+ * @author Juan
+ * @version January 2024
+ */
+public class Door extends Actor
+{
+    public Door()
+    {
+        GreenfootImage doorImage = new GreenfootImage("images/Door/door0.png");
+        doorImage.scale(140, 220);
+        this.setImage(doorImage);
+    }
+    
+    /**
+     * Act - do whatever the Door wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
